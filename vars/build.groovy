adfacf2e98c97e76ae7374bd7523f2fbadb803a9
nilanjanb3 <nilanjan.bhattacharjee77@gmail.com>
2023-04-07 19:55:50 +0530

    added method to install node js

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,3 +1,8 @@
+def installNodeJs(){
+    sh "sudo apt install nodejs"
+    sh "node_version=$(node -v)"
+    echo "Success installed Node.js ${node_version}"
+}
 def gitCheckout(url){
     checkout scmGit(branches: [[name: 'main']], extensions: [], userRemoteConfigs: [[url: "${url}"]])
     sh "Code Cloned Successfully !"
