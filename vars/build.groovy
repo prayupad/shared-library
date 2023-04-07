240a7c0e2b0a774ba5ebeae39ed725b13b0a3457
nilanjanb3 <nilanjan.bhattacharjee77@gmail.com>
2023-04-07 20:02:38 +0530

    added auto approval

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,10 +1,10 @@
 def installNodeJs(){
-    sh "sudo apt install nodejs"
+    sh "sudo apt install nodejs -y"
     // sh "node_version=$(node -v)"
     // echo "Success installed Node.js ${node_version}"
 }
 def installNpm(){
-    sh "sudo apt install npm"
+    sh "sudo apt install npm -y"
 }
 def gitCheckout(url){
     checkout scmGit(branches: [[name: 'main']], extensions: [], userRemoteConfigs: [[url: "${url}"]])
