2dbd8f9597a750e0616ce01ce1a45d9d3ae11e14
nilanjanb3 <nilanjan.bhattacharjee77@gmail.com>
2023-04-07 19:47:09 +0530

    added more methods

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,3 +1,14 @@
-def cloneSCM(url){
+def gitCheckout(url){
     checkout scmGit(branches: [[name: 'main']], extensions: [], userRemoteConfigs: [[url: "${url}"]])
+    sh "Code Cloned Successfully !"
+}
+
+def nodeJsBuild(){
+    sh "npm install"
+    echo "Build Successfully !"
+}
+
+def nodeJsTest(){
+    sh "npm run test"
+    echo "Test Successfully !"
 }
