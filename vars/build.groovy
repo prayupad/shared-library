1d6a5cde1ab327e43cf9204eeae533608f24b7ef
nilanjanb3 <nilanjan.bhattacharjee77@gmail.com>
2023-04-10 17:40:34 +0530

    fixed bugs

--- vars/build.groovy
+++ vars/build.groovy
@@ -29,7 +29,7 @@ def dockerBuildAndPush(dockerRegistry,credentialsId,imageName){
 }
 
 def testDSL(jobName){
-    jobDsl scriptText: '''pipelineJob(\"${jobName}\") {
+    jobDsl scriptText: '''pipelineJob() {
             parameters {
                 stringParam(\'repoURL\',\'\',\'Repository URL of the Project\' )
                 stringParam(\'dockerRegistry\',\'\', \'Docker Registry Login URL\' )
