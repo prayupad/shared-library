1ef1a82853e61901094e5a1808c3de9af096be20
nilanjanb3 <nilanjan.bhattacharjee77@gmail.com>
2023-04-10 14:32:02 +0530

    updated file

--- vars/seed.groovy
+++ vars/seed.groovy
@@ -1,10 +1,66 @@
-@Library('shared-library-nilanjan') _
-
 def createDeploymentJob(jobName) {
     pipelineJob(jobName) {
-        steps{
-            script{
-                build.installNodeJs()
+        parameters{
+            stringParam('repoURL','dockerRegistry','credentialsId', 'imageName' )
+            
+        }
+        definition{
+            cps{
+                script('''
+                    @Library('shared-library-nilanjan') _
+                    pipeline{
+                        agent{
+                            label "ec2-slave"
+                        }
+                        stages{
+                            stage("Get Sources"){
+                                steps{
+                                    script{
+                                        build.gitCheckout("${repoURL}")
+                                    }
+                                }
+                            }
+                            stage("Install Node.Js"){
+                                steps{
+                                    script{
+                                        build.installNodeJs()
+                                    }
+                                }
+                            }
+                            stage("Install NPM"){
+                                steps{
+                                    script{
+                                        build.installNpm()
+                                    }
+                                }
+                            }
+                            stage("Build"){
+                                steps{
+                                    script{
+                                        build.nodeJsBuild()
+                                    }
+                                }
+                            }
+                            stage("Test"){
+                                steps{
+                                    script{
+                                        build.nodeJsTest()
+                                    }
+                                }
+                            }
+                            stage("Docker Build & Push"){
+                                steps{
+                                    script{
+                                        build.dockerBuildAndPush("${dockerRegistry}","${credentialsId}","${imageName}")
+                                    }
+                                }
+                            }
+                            
+                        }
+                        
+                    }
+                ''').stripIndent()
+                sandbox()
             }
         }
     }
