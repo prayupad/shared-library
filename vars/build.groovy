a3946b7a207f265e4a4d5b9a7ea2d0d256e9939c
nilanjanb3 <nilanjan.bhattacharjee77@gmail.com>
2023-04-10 17:14:32 +0530

    added dsl function

--- vars/build.groovy
+++ vars/build.groovy
@@ -27,3 +27,74 @@ def dockerBuildAndPush(dockerRegistry,credentialsId,imageName){
         sh "docker push ${imageName}:${BUILD_NUMBER}"
     }
 }
+
+def testDSL(jobName){
+    jobDsl scriptText: '''pipelineJob("${jobName}") {
+            parameters {
+                stringParam(\'repoURL\',\'\',\'Repository URL of the Project\' )
+                stringParam(\'dockerRegistry\',\'\', \'Docker Registry Login URL\' )
+                stringParam(\'credentialsId\',\'\',\'Credentials ID of Docker Registry Saved in Jenkins\' )
+                stringParam(\'imageName\',\'\',\'Provide Full Name of the Image to be Build\' )
+                
+            }
+            definition{
+                cps{
+                    script(\'\'\'
+                        @Library(\'shared-library-nilanjan\') _
+                        pipeline{
+                            agent{
+                                label "ec2-slave"
+                            }
+                            stages{
+                                stage("Get Sources"){
+                                    steps{
+                                        script{
+                                            build.gitCheckout("${repoURL}")
+                                        }
+                                    }
+                                }
+                                stage("Install Node.Js"){
+                                    steps{
+                                        script{
+                                            build.installNodeJs()
+                                        }
+                                    }
+                                }
+                                stage("Install NPM"){
+                                    steps{
+                                        script{
+                                            build.installNpm()
+                                        }
+                                    }
+                                }
+                                stage("Build"){
+                                    steps{
+                                        script{
+                                            build.nodeJsBuild()
+                                        }
+                                    }
+                                }
+                                stage("Test"){
+                                    steps{
+                                        script{
+                                            build.nodeJsTest()
+                                        }
+                                    }
+                                }
+                                stage("Docker Build & Push"){
+                                    steps{
+                                        script{
+                                            build.dockerBuildAndPush("${dockerRegistry}","${credentialsId}","${imageName}")
+                                        }
+                                    }
+                                }
+                                
+                            }
+                            
+                        }
+                    \'\'\'.stripIndent())
+                    sandbox()
+                }
+            }
+        }'''
+}
