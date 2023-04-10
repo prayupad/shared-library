9fe002ccf609daccdba4567ad0480526fec8fedc
nilanjanb3 <nilanjan.bhattacharjee77@gmail.com>
2023-04-10 16:09:47 +0530

    added changes

--- vars/seed.groovy
+++ vars/seed.groovy
@@ -1,70 +1,73 @@
 def createDeploymentJob(jobName) {
-    pipelineJob("${jobName}") {
-        parameters {
-            stringParam('repoURL','','Repository URL of the Project' )
-          	stringParam('dockerRegistry','', 'Docker Registry Login URL' )
-          	stringParam('credentialsId','','Credentials ID of Docker Registry Saved in Jenkins' )
-          	stringParam('imageName','','Provide Full Name of the Image to be Build' )
-            
-        }
-        definition{
-            cps{
-                script('''
-                    @Library('shared-library-nilanjan') _
-                    pipeline{
-                        agent{
-                            label "ec2-slave"
-                        }
-                        stages{
-                            stage("Get Sources"){
-                                steps{
-                                    script{
-                                        build.gitCheckout("${repoURL}")
-                                    }
-                                }
+    jobDsl{
+        pipelineJob("${jobName}") {
+            parameters {
+                stringParam('repoURL','','Repository URL of the Project' )
+                stringParam('dockerRegistry','', 'Docker Registry Login URL' )
+                stringParam('credentialsId','','Credentials ID of Docker Registry Saved in Jenkins' )
+                stringParam('imageName','','Provide Full Name of the Image to be Build' )
+                
+            }
+            definition{
+                cps{
+                    script('''
+                        @Library('shared-library-nilanjan') _
+                        pipeline{
+                            agent{
+                                label "ec2-slave"
                             }
-                            stage("Install Node.Js"){
-                                steps{
-                                    script{
-                                        build.installNodeJs()
+                            stages{
+                                stage("Get Sources"){
+                                    steps{
+                                        script{
+                                            build.gitCheckout("${repoURL}")
+                                        }
                                     }
                                 }
-                            }
-                            stage("Install NPM"){
-                                steps{
-                                    script{
-                                        build.installNpm()
+                                stage("Install Node.Js"){
+                                    steps{
+                                        script{
+                                            build.installNodeJs()
+                                        }
                                     }
                                 }
-                            }
-                            stage("Build"){
-                                steps{
-                                    script{
-                                        build.nodeJsBuild()
+                                stage("Install NPM"){
+                                    steps{
+                                        script{
+                                            build.installNpm()
+                                        }
                                     }
                                 }
-                            }
-                            stage("Test"){
-                                steps{
-                                    script{
-                                        build.nodeJsTest()
+                                stage("Build"){
+                                    steps{
+                                        script{
+                                            build.nodeJsBuild()
+                                        }
                                     }
                                 }
-                            }
-                            stage("Docker Build & Push"){
-                                steps{
-                                    script{
-                                        build.dockerBuildAndPush("${dockerRegistry}","${credentialsId}","${imageName}")
+                                stage("Test"){
+                                    steps{
+                                        script{
+                                            build.nodeJsTest()
+                                        }
                                     }
                                 }
+                                stage("Docker Build & Push"){
+                                    steps{
+                                        script{
+                                            build.dockerBuildAndPush("${dockerRegistry}","${credentialsId}","${imageName}")
+                                        }
+                                    }
+                                }
+                                
                             }
                             
                         }
-                        
-                    }
-                '''.stripIndent())
-                sandbox()
+                    '''.stripIndent())
+                    sandbox()
+                }
             }
         }
     }
+    
 }
