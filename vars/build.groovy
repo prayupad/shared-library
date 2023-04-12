4537bdedaf8acc1d86fb54bafe246feb2cb35f7b
nilanjanb3 <nilanjan.bhattacharjee77@gmail.com>
2023-04-12 18:02:44 +0530

    added testDSL2

--- vars/build.groovy
+++ vars/build.groovy
@@ -99,3 +99,75 @@ def testDSL(name){
             }
         }'''.stripIndent()
 }
+
+def testDSL2(name, repoURL, imageName){
+    jobDsl scriptText: '''
+        pipelineJob('''+"\"${name}\""+''') {
+            parameters {
+                stringParam(\'repoURL\',\'\',\'Repository URL of the Project\' )
+                stringParam(\'dockerRegistry\',\'\', \'Docker Registry Login URL\' )
+                stringParam(\'credentialsId\',\'\',\'Credentials ID of Docker Registry Saved in Jenkins\' )
+                stringParam(\'imageName\',\'\',\'Provide Full Name of the Image to be Build\' )
+                
+            }
+            definition{
+                cps{
+                    script(\'\'\'
+                        @Library(\'shared-library-nilanjan\') _
+                        pipeline{
+                            agent{
+                                label "ec2-slave"
+                            }
+                            stages{
+                                stage("Get Sources"){
+                                    steps{
+                                        script{
+                                            build.gitCheckout('''+"\"${repoURL}\""+''')
+                                        }
+                                    }
+                                }
+                                stage("Install Node.Js"){
+                                    steps{
+                                        script{
+                                            build.installNodeJs()
+                                        }
+                                    }
+                                }
+                                stage("Install NPM"){
+                                    steps{
+                                        script{
+                                            build.installNpm()
+                                        }
+                                    }
+                                }
+                                stage("Build"){
+                                    steps{
+                                        script{
+                                            build.nodeJsBuild()
+                                        }
+                                    }
+                                }
+                                stage("Test"){
+                                    steps{
+                                        script{
+                                            build.nodeJsTest()
+                                        }
+                                    }
+                                }
+                                stage("Docker Build & Push"){
+                                    steps{
+                                        script{
+                                            build.dockerBuildAndPush("https://index.docker.io/v1/","nilanjan-docker",'''+"\"${imageName}\""+''')
+                                        }
+                                    }
+                                }
+                                
+                            }
+                            
+                        }
+                    \'\'\'.stripIndent())
+                    sandbox()
+                }
+            }
+        }'''.stripIndent()
+}
